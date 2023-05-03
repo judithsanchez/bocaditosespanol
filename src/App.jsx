336fd48f4e3ef042d7ff875e79c43abd379bd4c8
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-05-03 15:58:45 -0500

    Made the change color mode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import { useState } from 'react';
+import { useEffect } from 'react';
+
 import '/src/App.css';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.style.setProperty(
+        '--current-mode-box-shadow',
+        '0px 1px 4px -2px rgba(168, 168, 168, 0.25)'
+      );
+      document.documentElement.style.setProperty('--current-mode', '#333333');
+      document.documentElement.style.setProperty(
+        '--current-mode-text',
+        'white'
+      );
+    } else {
+      document.documentElement.style.setProperty(
+        '--current-mode-box-shadow',
+        '0px 3px 4px -2px rgba(0, 0, 0, 0.25)'
+      );
+      document.documentElement.style.setProperty('--current-mode', 'white');
+      document.documentElement.style.setProperty(
+        '--current-mode-text',
+        'black'
+      );
+    }
+  }, [isDarkMode]);
+
+  const toggleMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar toggleMode={toggleMode} isDarkMode={isDarkMode} />
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
 import '/src/components/NavBar/NavBar.css';
 
-function NavBar() {
+function NavBar({ toggleMode, isDarkMode }) {
+  const [modeButtonImg, setModeButtonImg] = useState(
+    '/src/components/NavBar/assets/button-to-dark-mode.svg'
+  );
+
+  const [mainLogoImg, setmainLogoImg] = useState(
+    '/src/components/NavBar/assets/logo-main-grey.svg'
+  );
+
+  const handleModeChange = () => {
+    toggleMode();
+    if (!isDarkMode) {
+      setModeButtonImg(
+        '/src/components/NavBar/assets/button-to-light-mode.svg'
+      );
+      setmainLogoImg('/src/components/NavBar/assets/logo-main-white.svg');
+    } else {
+      setModeButtonImg('/src/components/NavBar/assets/button-to-dark-mode.svg');
+      setmainLogoImg('/src/components/NavBar/assets/logo-main-grey.svg');
+    }
+  };
+
   return (
     <nav className="NavBar">
-      <img
-        src="/src/components/NavBar/assets/logo-main-grey.svg"
-        alt="Main Bocaditos Logo"
-      />
+      <img src={mainLogoImg} alt="Main Bocaditos Logo" />
       <ul>
         <li>Lessons</li>
         <li>Games</li>
         <li>Practice</li>
       </ul>
       <img
-        src="/src/components/NavBar/assets/button-to-dark-mode.svg"
-        alt="Dark Mode Button"
+        src={modeButtonImg}
+        alt="Mode Toggle Button"
+        onClick={handleModeChange}
       />
     </nav>
   );
