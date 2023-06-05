a2e25f4aab796f8c2e25e962b7f7b323188ce58e
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-05 13:04:49 -0500

    Replaces subscription form

--- src/components/SubscriptionForm.jsx
+++ src/components/SubscriptionForm.jsx
@@ -1,56 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import config from './config';
-
+import React from 'react';
 import '/src/components/styles/SubscriptionForm.css';
 
 function SubscriptionForm() {
-  const [email, setEmail] = useState('');
-  const [isSubscribed, setIsSubscribed] = useState(false);
-
-  const apiKey = config.apiKey;
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const contact = {
-      email,
-      listIds: [3],
-    };
-
-    axios
-      .post('https://api.brevo.com/v3/contacts', contact, {
-        headers: {
-          'api-key': apiKey,
-        },
-      })
-      .then((response) => {
-        console.log('Contact created:', response.data);
-        setIsSubscribed(true);
-      })
-      .catch((error) => {
-        console.error('Error creating contact:', error);
-      });
-  }
-
   return (
-    <form className="SubscriptionForm" onSubmit={handleSubmit}>
-      <div className="addEmail">
-        <input
-          placeholder="youremail@domain.com"
-          type="text"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <button type="submit">
-          <img
-            src="https://cdn.bfldr.com/Z0BJ31FP/at/v9gfq3743pshr5vz83rgxg4j/icon-envelope.svg"
-            alt="Envelope Icon"
-          />
-        </button>
-      </div>
-      {isSubscribed && <p>You are subscribed! ✅</p>}{' '}
-    </form>
+    <div className="SubscriptionForm">
+      <a href="https://fc6a2b43.sibforms.com/serve/MUIEAK3OOTkmgTbF3ByIeSfi5wDSsHwWIWD3ers5frOIw9rXRX1IB4WrJ77STOOxDpNa5zYLKZP7OnHG9r_PsnPb8FY_10uiIHoIpA4TuRaMdCFKdKJ-nS5O5KJ8tgmKbnqpy8fJMxUJ08wLOw95yLsVocILsLGvWs0Gy7hBsKGcnQ-oCQLEl1rRCihpCzW5paoQXDXTJbGRf6ub">
+        Subscribe!
+      </a>
+      <img
+        src="https://cdn.bfldr.com/Z0BJ31FP/at/v9gfq3743pshr5vz83rgxg4j/icon-envelope.svg"
+        alt="Envelope Icon"
+      />
+    </div>
   );
 }
 
