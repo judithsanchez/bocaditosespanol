4e9dd476552a86459fed3d30ee44b7ebce1f0a17
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-07 11:31:51 -0500

    Made improvements on the lesson page

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -38,7 +38,67 @@ function Lesson() {
 
   return (
     <div className="LessonPage">
-      <h1>{lesson?.subject}</h1>
+      <div className="title-progressBar-container">
+        <h1>{lesson?.subject}</h1>
+        {/* <ProgressBar></ProgressBar> */}
+      </div>
+
+      <div className="keyPoints-container">
+        <h3 className="keyPointsTitle">🔑 Key Points</h3>
+        <div className="keys">
+          {keyPoints.map((point, index) => (
+            <p className="point" key={index}>
+              {point}
+            </p>
+          ))}
+        </div>
+      </div>
+
+      <div className="examples-teacher-container">
+        <h3 className="exampleTitle">💡 Examples</h3>
+
+        <div className="examples">
+          {examples.map((example, index) => (
+            <p key={index}>{example}</p>
+          ))}
+        </div>
+
+        <div className="teacher-container">
+          <img
+            className="teacher"
+            src="https://cdn.bfldr.com/Z0BJ31FP/at/r8vb8ts4p9vvfk6q4v7qn8sg/teacher-intro.svg"
+            alt="Teacher explaining"
+          />
+        </div>
+
+        <div className="arrow-left">
+          {currentStep > 0 && (
+            <img
+              className="navigation-arrow"
+              src="https://cdn.bfldr.com/Z0BJ31FP/at/498n249kr6fqfbrtcgws77m6/icon-previous-arrow.svg"
+              alt="Icon previous arrow"
+              onClick={() => previousStep()}
+            />
+          )}
+        </div>
+
+        <div className="explanation">
+          <p>{lesson.steps[currentStep].explanation}</p>
+        </div>
+
+        <div className="arrow-right">
+          {currentStep !== lesson.steps.length - 1 && (
+            <img
+              className="navigation-arrow"
+              src="https://cdn.bfldr.com/Z0BJ31FP/as/2s36qjvpjsggrbgp9m3vt/icon-next-arrow?auto=webp&format=png"
+              alt="Icon next arrow"
+              onClick={() => nextStep()}
+            />
+          )}
+        </div>
+      </div>
+
+      {/* <h1>{lesson?.subject}</h1>
       <div className="body">
         <div className="explanation">
           <p>{lesson.steps[currentStep].explanation}</p>
@@ -91,7 +151,7 @@ function Lesson() {
             />
           )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
