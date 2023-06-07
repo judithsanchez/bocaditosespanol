1658b86836eaaa29ee2d0c3301a6b46f16238319
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-07 16:25:48 -0500

    Implemented no responsive logic

---
+++ src/components/NoResponsive.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import '/src/components/styles/NoResponsive.css';
+
+function NoResponsive() {
+  return (
+    <div className="NoResponsive">
+      <div className="overlay">
+        <img
+          className="broken-glass-image"
+          src="https://cdn.bfldr.com/Z0BJ31FP/at/8tsp46fjmrsmc5r78s3kc/broken-glass-light-mode.svg"
+          alt="Broken screen"
+        />
+      </div>
+
+      <img
+        className="evil-bruja-image"
+        src="https://cdn.bfldr.com/Z0BJ31FP/at/9frphmfh5mbr8p6njhzm6r7/no-horizontal-mode.svg"
+        alt="Evil Bruja"
+      />
+      <p>
+        ⚠️ Watch out for Webby, the malevolent Internet Witch! She sometimes
+        haunts the landscape mode of small devices, causing chaos and mischief.
+        Stay safe and stick to portrait mode. ⚠️
+      </p>
+    </div>
+  );
+}
+
+export default NoResponsive;
--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -10,6 +10,7 @@ import '/src/pages/styles/Lesson.css';
 // Components
 
 import ProgressBar from '../components/ProgressBar';
+import NoResponsive from '../components/NoResponsive';
 
 function Lesson() {
   const { slug } = useParams();
@@ -53,70 +54,74 @@ function Lesson() {
   }, [currentStep, lesson]);
 
   return (
-    <div className="LessonPage">
-      <div className="title-progressBar-container">
-        <h1>{lesson?.subject}</h1>
-        <ProgressBar
-          currentStep={currentStep}
-          totalSteps={lesson?.steps.length - 1}
-        />
-      </div>
+    <>
+      <NoResponsive />
 
-      <div className="keyPoints-container">
-        <h3 className="keyPointsTitle">🔑 Key Points</h3>
-        <div className="keys">
-          {keyPoints.map((point, index) => (
-            <p className="point" key={index}>
-              {point}
-            </p>
-          ))}
-        </div>
-      </div>
-
-      <div className="examples-teacher-container">
-        <h3 className="exampleTitle">💡 Examples</h3>
-
-        <div className="examples">
-          {examples.map((example, index) => (
-            <p key={index}>{example}</p>
-          ))}
-        </div>
-
-        <div className="teacher-container">
-          <img
-            className="teacher"
-            src="https://cdn.bfldr.com/Z0BJ31FP/at/r8vb8ts4p9vvfk6q4v7qn8sg/teacher-intro.svg"
-            alt="Teacher explaining"
+      <div className="LessonPage">
+        <div className="title-progressBar-container">
+          <h1>{lesson?.subject}</h1>
+          <ProgressBar
+            currentStep={currentStep}
+            totalSteps={lesson?.steps.length - 1}
           />
         </div>
 
-        <div className="arrow-left">
-          {currentStep > 0 && (
-            <img
-              className="navigation-arrow"
-              src="https://cdn.bfldr.com/Z0BJ31FP/at/498n249kr6fqfbrtcgws77m6/icon-previous-arrow.svg"
-              alt="Icon previous arrow"
-              onClick={() => previousStep()}
-            />
-          )}
+        <div className="keyPoints-container">
+          <h3 className="keyPointsTitle">🔑 Key Points</h3>
+          <div className="keys">
+            {keyPoints.map((point, index) => (
+              <p className="point" key={index}>
+                {point}
+              </p>
+            ))}
+          </div>
         </div>
 
-        <div className="explanation">
-          <p>{lesson.steps[currentStep].explanation}</p>
-        </div>
+        <div className="examples-teacher-container">
+          <h3 className="exampleTitle">💡 Examples</h3>
 
-        <div className="arrow-right">
-          {currentStep !== lesson.steps.length - 1 && (
+          <div className="examples">
+            {examples.map((example, index) => (
+              <p key={index}>{example}</p>
+            ))}
+          </div>
+
+          <div className="teacher-container">
             <img
-              className="navigation-arrow"
-              src="https://cdn.bfldr.com/Z0BJ31FP/as/2s36qjvpjsggrbgp9m3vt/icon-next-arrow?auto=webp&format=png"
-              alt="Icon next arrow"
-              onClick={() => nextStep()}
+              className="teacher"
+              src="https://cdn.bfldr.com/Z0BJ31FP/at/r8vb8ts4p9vvfk6q4v7qn8sg/teacher-intro.svg"
+              alt="Teacher explaining"
             />
-          )}
+          </div>
+
+          <div className="arrow-left">
+            {currentStep > 0 && (
+              <img
+                className="navigation-arrow"
+                src="https://cdn.bfldr.com/Z0BJ31FP/at/498n249kr6fqfbrtcgws77m6/icon-previous-arrow.svg"
+                alt="Icon previous arrow"
+                onClick={() => previousStep()}
+              />
+            )}
+          </div>
+
+          <div className="explanation">
+            <p>{lesson.steps[currentStep].explanation}</p>
+          </div>
+
+          <div className="arrow-right">
+            {currentStep !== lesson.steps.length - 1 && (
+              <img
+                className="navigation-arrow"
+                src="https://cdn.bfldr.com/Z0BJ31FP/as/2s36qjvpjsggrbgp9m3vt/icon-next-arrow?auto=webp&format=png"
+                alt="Icon next arrow"
+                onClick={() => nextStep()}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
