93a2a2d8bbea340d2a0097dda4a83f659548f6eb
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-07 18:06:55 -0500

    Added analytics tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+import App from './App.jsx';
+import './index.css';
+
+// Initialize Google Analytics
+ReactGA.initialize('G-R7QVHQ6X18');
+
+// Track initial pageview
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+// Render the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
