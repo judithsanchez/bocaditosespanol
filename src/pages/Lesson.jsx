f09868fbf9c784ae0fe2b52c9a14ca996467f451
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-06-07 14:03:43 -0500

    Fixed keypoints bug

--- src/pages/Lesson.jsx
+++ src/pages/Lesson.jsx
@@ -32,7 +32,19 @@ function Lesson() {
 
   useEffect(() => {
     const currentStepObject = lesson?.steps[currentStep];
-    setKeyPoints(currentStepObject?.keys || []);
+    const currentStepKeyPoints = currentStepObject?.keys || [];
+
+    const accumulatedKeyPoints = lesson.steps
+      .slice(0, currentStep)
+      .reduce(
+        (accumulated, step) => [...accumulated, ...(step.keys || [])],
+        []
+      );
+
+    setKeyPoints(
+      Array.from(new Set([...accumulatedKeyPoints, ...currentStepKeyPoints]))
+    );
+
     setExamples(currentStepObject?.examples || []);
   }, [currentStep, lesson]);
 
@@ -97,61 +109,6 @@ function Lesson() {
           )}
         </div>
       </div>
-
-      {/* <h1>{lesson?.subject}</h1>
-      <div className="body">
-        <div className="explanation">
-          <p>{lesson.steps[currentStep].explanation}</p>
-        </div>
-
-        <div className="content">
-          <div className="keys">
-            <h3>Key Points</h3>
-            {keyPoints.map((point, index) => (
-              <p className="point" key={index}>
-                {point}
-              </p>
-            ))}
-          </div>
-          <div className="examples">
-            <h3>Examples</h3>
-            {examples.map((example, index) => (
-              <p className="example" key={index}>
-                {example}
-              </p>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div className="teacher-container">
-        <div className="arrow-container">
-          {currentStep > 0 && (
-            <img
-              className="navigation-arrow"
-              src="https://cdn.bfldr.com/Z0BJ31FP/at/498n249kr6fqfbrtcgws77m6/icon-previous-arrow.svg"
-              alt="Icon previous arrow"
-              onClick={() => previousStep()}
-            />
-          )}
-        </div>
-
-        <img
-          className="teacher-image"
-          src="https://cdn.bfldr.com/Z0BJ31FP/at/r8vb8ts4p9vvfk6q4v7qn8sg/teacher-intro.svg"
-          alt="Teacher explaining"
-        />
-
-        <div className="arrow-container">
-          {currentStep !== lesson.steps.length - 1 && (
-            <img
-              className="navigation-arrow"
-              src="https://cdn.bfldr.com/Z0BJ31FP/as/2s36qjvpjsggrbgp9m3vt/icon-next-arrow?auto=webp&format=png"
-              alt="Icon next arrow"
-              onClick={() => nextStep()}
-            />
-          )}
-        </div>
-      </div> */}
     </div>
   );
 }
