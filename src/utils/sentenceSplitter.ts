b91fc52e40bcc8b16b5880d8df095495dc8c6c5e
judithsanchez <judithv.sanchezc@gmail.com>
2024-02-17 00:49:40 -0600

    feat: :construction:

--- src/utils/sentenceSplitter.ts
+++ src/utils/sentenceSplitter.ts
@@ -68,7 +68,7 @@ class TextProcessor {
 		}
 
 		const result = sentences.map(sentence => ({
-			tokens: this.processWords(sentence),
+			tokens: this.tokenizeSentence(sentence),
 			sentence: sentence,
 		}));
 		return result;
@@ -78,11 +78,48 @@ class TextProcessor {
 		return text.split(/([.?!¡¿, ])/).filter(token => token.trim() !== '');
 	}
 
-	private processWords(words: string[]): IToken[] {
-		return words.map(word => ({
-			token: word,
-			isPunctuationSign: this.isPunctuation(word),
-		}));
+	private tokenizeSentence(sentence: string): IToken[] {
+		const words = sentence.split(' ');
+		const tokenizedSentence: IToken[] = [];
+
+		for (const word of words) {
+			const tokens = this.tokenizeWord(word);
+
+			for (const token of tokens) {
+				const isPunctuationSign = this.isPunctuation(token);
+
+				const tokenObject: IToken = {
+					token,
+					isPunctuationSign,
+				};
+
+				tokenizedSentence.push(tokenObject);
+			}
+		}
+		return tokenizedSentence;
+	}
+
+	private tokenizeWord(word: string): string[] {
+		const tokenizedWord: string[] = [];
+		let currentToken = '';
+
+		for (const char of word) {
+			if (this.isPunctuation(char)) {
+				if (currentToken.length > 0) {
+					tokenizedWord.push(currentToken);
+					currentToken = '';
+				}
+				tokenizedWord.push(char);
+			} else {
+				currentToken += char;
+			}
+		}
+
+		if (currentToken.length > 0) {
+			tokenizedWord.push(currentToken);
+		}
+
+		return tokenizedWord;
 	}
 
 	private isPunctuation(char: string): boolean {
@@ -97,86 +134,3 @@ console.log(testSentence.processedText[0]);
 console.log(testSentence.processedText[2]);
 
 export {ISentence, TextProcessor};
-
-// class TextProcessor {
-// 	processedText: ISentence[];
-
-// 	constructor(text: string) {
-// 		if (typeof text !== 'string') {
-// 			throw new Error('TextProcessor can only be initialized with a string.');
-// 		}
-
-// 		if (text.trim().length === 0) {
-// 			throw new Error(
-// 				'TextProcessor cannot be initialized with an empty string.',
-// 			);
-// 		}
-
-// 		this.processedText = this.processText(text);
-// 	}
-
-// 	private processText(text: string): ISentence[] {
-// 		const sentences = this.tokenizeSentences(text);
-// 		if (sentences === null) {
-// 			throw new Error('No valid sentences found in the input text.');
-// 		}
-
-// 		const result = sentences.map(sentence => ({
-// 			tokens: this.tokenizeSentence(sentence),
-// 			sentence: sentence,
-// 		}));
-// 		return result;
-// 	}
-
-// 	private tokenizeSentences(text: string): string[] | null {
-// 		return text.split(/([.?!¡¿, ])/).filter(token => token.trim() !== '');
-// 	}
-
-// 	private tokenizeSentence(sentence: string): IToken[] {
-// 		const words = sentence.split(' ');
-// 		const tokenizedSentence: IToken[] = [];
-
-// 		for (const word of words) {
-// 			const tokens = this.tokenizeWord(word);
-
-// 			for (const token of tokens) {
-// 				const isPunctuationSign = this.isPunctuation(token);
-
-// 				const tokenObject: IToken = {
-// 					token,
-// 					isPunctuationSign,
-// 				};
-
-// 				tokenizedSentence.push(tokenObject);
-// 			}
-// 		}
-// 		return tokenizedSentence;
-// 	}
-
-// 	private tokenizeWord(word: string): string[] {
-// 		const tokenizedWord: string[] = [];
-// 		let currentToken = '';
-
-// 		for (const char of word) {
-// 			if (this.isPunctuation(char)) {
-// 				if (currentToken.length > 0) {
-// 					tokenizedWord.push(currentToken);
-// 					currentToken = '';
-// 				}
-// 				tokenizedWord.push(char);
-// 			} else {
-// 				currentToken += char;
-// 			}
-// 		}
-
-// 		if (currentToken.length > 0) {
-// 			tokenizedWord.push(currentToken);
-// 		}
-
-// 		return tokenizedWord;
-// 	}
-
-// 	private isPunctuation(char: string): boolean {
-// 		return punctuationSigns.has(char);
-// 	}
-// }
