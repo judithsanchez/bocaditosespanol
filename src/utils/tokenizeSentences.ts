75c3717feff096a1670e509e3c50176aa35079c6
judithsanchez <judithv.sanchezc@gmail.com>
2024-05-15 21:32:03 -0600

    refactor(types): :recycle: Update function to account for the new interfaces

--- src/utils/tokenizeSentences.ts
+++ src/utils/tokenizeSentences.ts
@@ -1,16 +1,28 @@
 /**
- * Tokenizes a sentence into an array of strings.
+ * Tokenizes a sentence into an array of tokens.
  *
- * A token is a meaningful unit of text, typically a word, an emoji or a punctuation mark,
- * that is obtained by splitting a sentence into smaller pieces.
+ * A token can be a word, an emoji, or a punctuation sign. Each token is represented
+ * as an object with properties `token` and `type`.
+ *
+ * For words, the `token` property is an object with the following properties:
+ * - `spanish`: The original word in Spanish.
+ * - `normalizedToken`: The word in its normalized form (lowercase, without accents).
+ * - `hasSpecialChar`: A boolean indicating whether the original word contained special characters.
+ *
+ * For emojis and punctuation signs, the `token` property is a string representing the emoji or punctuation sign.
+ *
+ * The `type` property indicates the type of the token (word, emoji, or punctuation sign).
+ *
  * @param sentence - The sentence to be tokenized.
  * @throws {TypeError} If the input is not a string.
  * @throws {Error} If the sentence is empty after trimming.
  * @returns An object containing the original sentence and an array of tokens.
  */
+
 import emojiRegex from 'emoji-regex';
-import {ISentence} from '../lib/types';
+import {ISentence, IToken, TokenType} from '../lib/types';
 import {errors} from '../lib/constants';
+import {normalizeString} from './normalizeString';
 
 const tokenizeSentences = (sentence: string): ISentence => {
 	if (typeof sentence !== 'string') {
@@ -25,9 +37,25 @@ const tokenizeSentences = (sentence: string): ISentence => {
 	const emojiPattern = emojiRegex();
 	const pattern = `(${emojiPattern.source}|\\.{3}|[.?!¡¿,:;'"\\s-])`;
 	const regex = new RegExp(pattern, 'gu');
-	const tokens: string[] = trimmedSentence
+	const tokens: IToken[] = trimmedSentence
 		.split(regex)
-		.filter(token => token.trim() !== '');
+		.filter(token => token.trim() !== '')
+		.map(token => {
+			if (emojiPattern.test(token)) {
+				return {token, type: TokenType.Emoji};
+			} else if (/^[.?!¡¿,:;'"\\s-]+$/.test(token)) {
+				return {token, type: TokenType.PunctuationSign};
+			} else {
+				const spanish = token;
+				const normalizedToken = normalizeString(spanish);
+				const hasSpecialChar = spanish !== normalizedToken;
+				return {
+					token: {spanish, normalizedToken, hasSpecialChar},
+					type: TokenType.Word,
+				};
+			}
+		});
+
 	return {
 		sentence: trimmedSentence,
 		tokens: tokens,
