487b60482af238a10cd2f5a499d36ea43f6d9e16
judithsanchez <judithv.sanchezc@gmail.com>
2025-01-18 16:58:42 +0100

    🐛  fix (deployment): comment out config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const API_URL =
-	process.env.NODE_ENV === 'development'
-		? 'http://localhost:3000'
-		: 'https://bocaditosespanol-backend.vercel.app';
+// export const API_URL =
+// 	process.env.NODE_ENV === 'development'
+// 		? 'http://localhost:3000'
+// 		: 'https://bocaditosespanol-backend.vercel.app';
