4061ea198c0bcbab55a15d08d09e4ab31c45a2b8
judithsanchez <judithv.sanchezc@gmail.com>
2025-01-19 20:39:03 +0100

    🐛  fix (deployment): app listen changes

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -45,10 +45,10 @@ const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
 
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === 'development') {
-	app.listen(port, () => {
-		logger.info('Development server started', {port});
-	});
-}
+// if (process.env.NODE_ENV === 'development') {
+app.listen(port, () => {
+	logger.info('Development server started', {port});
+});
+// }
 
 export default (req: Request, res: Response) => app(req, res);
